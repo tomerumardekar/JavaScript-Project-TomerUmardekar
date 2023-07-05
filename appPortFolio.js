914454ae1f4300b41b48348d03f085754b878f14
tomerumardekar <tomtom1361991@gmail.com>
2023-07-05 14:21:08 +0300

    i add animation

--- appPortFolio.js
+++ appPortFolio.js
@@ -15,3 +15,14 @@ setInterval(() => {
 
   index = index + 1;
 }, 400);
+
+window.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector(".header");
+  const container = document.querySelector(".container");
+
+  // Add "fade-in" class to the header and container elements after a delay
+  setTimeout(() => {
+    header.classList.add("fade-in");
+    container.classList.add("fade-in");
+  }, 1000);
+});
